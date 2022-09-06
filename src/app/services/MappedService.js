2875465838a5b9b397aa0b448d26e9ebd67feab1
yuridavello <yuridavello@hotmail.com>
2022-09-06 20:04:12 -0300

    arrumando rota de produto mapeado

--- src/app/services/MappedService.js
+++ src/app/services/MappedService.js
@@ -3,8 +3,6 @@ import Sector from "../models/Sector";
 import Floor from "../models/Floor";
 import Shelf from "../models/Shelf";
 
-const { Op } = require("sequelize");
-
 class MappedService {
   async fullMap(productId) {
     const product = await Product.findByPk(productId);
@@ -12,21 +10,21 @@ class MappedService {
 
     const sectors = await Sector.findAll({
       where: {
-        product_id: parseInt(productId),
+        productId,
       },
     });
     if (!sectors) return false;
 
     const floors = await Floor.findAll({
       where: {
-        id: {[Op.in]: sectors.map(s => s.floorId)},
+        id: sectors[0].floorId,
       },
     });
     if (!floors) return false;
 
     const shelf = await Shelf.findAll({
       where: {
-        id: {[Op.in]: floors.map(f => f.shelfId)},
+        id: floors[0].shelfId,
       },
     });
     if (!shelf) return false;
