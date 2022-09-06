4901d684bd27b6b7bc83edcc9bf610103205f136
LuanPetruitis <luanpetruitis@hotmail.com>
2022-09-06 09:05:15 -0300

    Ajuste na rota mappedProduct

--- src/app/services/MappedService.js
+++ src/app/services/MappedService.js
@@ -3,36 +3,38 @@ import Sector from "../models/Sector";
 import Floor from "../models/Floor";
 import Shelf from "../models/Shelf";
 
+const { Op } = require("sequelize");
+
 class MappedService {
   async fullMap(productId) {
     const product = await Product.findByPk(productId);
     if (!product) return false;
 
-    const sector = await Sector.findAll({
+    const sectors = await Sector.findAll({
       where: {
-        productId,
+        product_id: parseInt(productId),
       },
     });
-    if (!sector) return false;
+    if (!sectors) return false;
 
-    const floor = await Floor.findAll({
+    const floors = await Floor.findAll({
       where: {
-        id: sector.floorId,
+        id: {[Op.in]: sectors.map(s => s.floorId)},
       },
     });
-    if (!floor) return false;
+    if (!floors) return false;
 
     const shelf = await Shelf.findAll({
       where: {
-        id: floor.shelfId,
+        id: {[Op.in]: floors.map(f => f.shelfId)},
       },
     });
     if (!shelf) return false;
 
     return {
       product,
-      sector,
-      floor,
+      sectors,
+      floors,
       shelf,
     };
   }
